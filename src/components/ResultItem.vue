2080ecf788ec808b84e7220a2bb5dd66f6a9b2ca
aronsantha <santhaaron@gmail.com>
2022-10-26 16:33:30 +0200

    Expanded Search functionality
    - search on keyup (removed submit button)
    - debounce by 500ms,
    - axios call,
    - error handling
    
    Other:
    - added comments
    - removed console.logs
    - aligned names for vars, props, emits
    
    Note1: "const api" is an empty string for security reasons.
    Note2: current UI styling and UI messages are temporary.

--- src/components/ResultItem.vue
+++ src/components/ResultItem.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="bg-red-200 p-5 outline outline-1">
+  <div class="bg-red-200 p-5 outline">
     <div>{{ movie.original_title }}</div>
     Rating: {{ movie.vote_average }}
-    <div>{{ movie.overview }}</div>
+    <div class="flex">
+      <img
+        :src="'https://image.tmdb.org/t/p/original' + movie.poster_path"
+        v-if="movie.poster_path"
+        class="w-40"
+      />
+      <img v-else class="w-40 h-60" />
+
+      <div>{{ movie.overview }}</div>
+    </div>
   </div>
 </template>
 
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,33 +1,80 @@
 <template>
   <div class="bg-blue-200 p-2 flex gap-5">
     <input
+      @keyup="debounceGetMovies"
       v-model="search"
       type="string"
       placeholder="Search for movies"
       class="text-3xl p-2"
       aria-label="Movie title"
     />
-    <button @click="getMovies">Search</button>
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
 import { ref } from "vue";
+const emit = defineEmits(["emitResponseData", "emitErrorMsg", "emitLoader"]);
 
-const emit = defineEmits(["toParent"]);
+// Search function
+
 const search = ref(null);
+const api = "";
 
 function getMovies() {
-  axios
-    .get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-US&query=${search.value}&page=1&include_adult=false`
-    )
-    .then((response) => {
-      emit("toParent", response.data.results);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  if (search.value.length > 2) {
+    // IF MORE THAN 2 CHARS: Clear previous emits, Start loader, Initiate axios call
+    emit("emitErrorMsg", "");
+    emit("emitResponseData", []);
+    emit("emitLoader", true);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-US&query=${search.value}&page=1&include_adult=false`
+      )
+      .then((response) => {
+        // IF NO RESULT: emmitting custom message (because API does not send error message in that case)
+        if (response.data.total_results < 1) {
+          emit("emitErrorMsg", "Sorry, no results for your search!");
+        }
+
+        // IF RESULT: emmitting result
+        emit("emitResponseData", response.data.results);
+      })
+      .catch((error) => {
+        // IF API ERROR: emmitting API's error
+        if (error.response && error.response.data) {
+          emit("emitErrorMsg", error.response.data.status_message);
+        } else {
+          emit(
+            "emitErrorMsg",
+            "Oh no, it looks like there has been an error: " +
+              error.message +
+              " !"
+          );
+        }
+      })
+      .finally(() => {
+        // WHEN ALL DONE: turn off loader
+        emit("emitLoader", false);
+      });
+  } else {
+    // IF EMPTY INPUT: empty ResponseData accordingly
+    emit("emitResponseData", "");
+    emit("emitLoader", false);
+  }
 }
+
+// Debounce function
+
+function debounce(func, timeout = 500) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+const debounceGetMovies = debounce(() => getMovies());
 </script>
