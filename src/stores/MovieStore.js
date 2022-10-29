6bce140635dfdafd1812a747a12e76370f4718c0
aronsantha <santhaaron@gmail.com>
2022-10-29 11:22:21 +0200

    Updated Global State management
    - Rewrote the store following "Setup" syntax.
    Read more about Setup Stores: https://pinia.vuejs.org/core-concepts/#setup-stores
    
    - Added local storage. It works like this:
    1. local storage populates the store 2. the store updates the local storage

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,32 +1,50 @@
-import { defineStore } from "pinia";
+// Pinia store following "Setup" syntax
+// Read more: https://pinia.vuejs.org/core-concepts/#setup-stores
 
-export const useMovieStore = defineStore("MovieStore", {
-  state() {
-    return {
-      savedMovies: [],
-    };
-  },
-  actions: {
-    addMovie(movie) {
-      // check if movie is already saved
-      const isSaved = this.savedMovies.some((object) => {
-        if (object.id === movie.id) {
-          return true;
-        }
-        return false;
-      });
-      // if NOT saved, push movie
-      if (!isSaved) {
-        this.savedMovies.push(movie);
+import { defineStore } from "pinia";
+import { ref, watch, computed, reactive } from "vue";
+
+export const useMovieStore = defineStore("MovieStore", () => {
+  const savedMovies = ref([]);
+
+  // If localMovies exists in localStorage, update store
+
+  if (localStorage.getItem("localMovies")) {
+    savedMovies.value = JSON.parse(localStorage.getItem("localMovies"));
+  }
+
+  // If store changes, update localMovies
+
+  watch(savedMovies.value, setLocalMovies);
+
+  function setLocalMovies() {
+    localStorage.setItem("localMovies", JSON.stringify(savedMovies.value));
+  }
+
+  // ACTIONS
+
+  function addMovie(movie) {
+    // check if movie is already saved
+    const isSaved = savedMovies.value.some((object) => {
+      if (object.id === movie.id) {
+        return true;
       }
-    },
-    removeMovie(removeId) {
-      // get index of object with id of removeId
-      const removeIndex = this.savedMovies.findIndex((object) => {
-        return object.id === removeId;
-      });
-      // remove object from savedMovies
-      this.savedMovies.splice(removeIndex, 1);
-    },
-  },
+      return false;
+    });
+    // if NOT saved, push movie
+    if (!isSaved) {
+      savedMovies.value.push(movie);
+    }
+  }
+
+  function removeMovie(removeId) {
+    // get index of object with id of removeId
+    const removeIndex = savedMovies.value.findIndex((object) => {
+      return object.id === removeId;
+    });
+    // remove object from savedMovies
+    savedMovies.value.splice(removeIndex, 1);
+  }
+
+  return { savedMovies, addMovie, removeMovie, setLocalMovies };
 });
